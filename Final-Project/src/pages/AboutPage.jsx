fe189c92d241960f43eeb3824f40ffcf7a82cad4
Nova Firmansyah <111202012881@mhs.dinus.ac.id>
2023-06-11 23:55:01 +0700

    menambahkan about

--- Final-Project/src/pages/AboutPage.jsx
+++ Final-Project/src/pages/AboutPage.jsx
@@ -1,6 +1,22 @@
-const AboutPage = () => {
-  return <div>AboutPage</div>;
+import {Container, Row, Col} from "react-bootstrap"
+import Faq from "../components/Faq";
 
+
+const AboutPage = () => {
+  return (
+  <div className="about-page">
+    <div className="about min-vh-100">
+      <Container>
+        <Row>
+          <Col>
+          <h1 className="fw-bold text-center mb-2 ">About</h1>
+          <p className="text-center"> Tentang kami</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </div>
+  );
 };
 
 export default AboutPage;
