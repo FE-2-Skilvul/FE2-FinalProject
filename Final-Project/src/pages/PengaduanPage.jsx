b1cfe9d6d4cd69fbd4faf7cb340611d28b364b3d
Nova Firmansyah <111202012881@mhs.dinus.ac.id>
2023-06-11 22:25:04 +0700

    menambah halaman pengaduan

--- Final-Project/src/pages/PengaduanPage.jsx
+++ Final-Project/src/pages/PengaduanPage.jsx
@@ -1,6 +1,24 @@
+import {Container, Row, Col} from 'react-bootstrap'
+
 const PengaduanPage = () => {
-  return <div>PengaduanPage</div>;
+  return(
+  <div className='pengaduan'>
+    <Container>
+      <Row className='mb-10'>
+        <Col>
+          <h1 className='text-center fw-bold'>Form Pengaduan</h1>
+          <p className='text-center'>Mohon isi formulir berikut sesuai dengan pelanggaran lingkungan yang terjadi di daerah kamu</p>
+        </Col>
+      </Row>
+      <Row>
+
+        {/* ISI SAMA FORM YANG UDAH KAMU BUAT */}
+
+      </Row>
+    </Container>
+  </div>
   
+  );
 };
 
 export default PengaduanPage;
