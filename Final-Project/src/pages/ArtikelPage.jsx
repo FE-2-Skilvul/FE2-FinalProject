a84b070288541555962f0656ffa0f331fce61b0f
aninisnaini <aninisnaini214@gmail.com>
2023-06-13 13:28:07 +0700

    menambahkan pengaduan dan artikel

--- Final-Project/src/pages/ArtikelPage.jsx
+++ Final-Project/src/pages/ArtikelPage.jsx
@@ -1,39 +1,159 @@
-import {Container, Row, Col} from "react-bootstrap"
-import { semuaArtikel } from "../data";
+import { Container, Row, Col } from "react-bootstrap";
+import Artikel1 from "../assets/img/artikel/edukasi-1.jpg";
+import Artikel2 from "../assets/img/artikel/edukasi2.jpg";
+import Artikel3 from "../assets/img/artikel/edukasi3.jpg";
+import Artikel4 from "../assets/img/artikel/edukasi4.jpg";
+import Artikel5 from "../assets/img/artikel/edukasi5.jpg";
+import Artikel6 from "../assets/img/artikel/edukasi6.jpg";
+import Artikel7 from "../assets/img/artikel/edukasi7.jpg";
+import Artikel8 from "../assets/img/artikel/edukasi8.jpg";
+import Artikel9 from "../assets/img/artikel/edukasi9(1).jpeg";
+import Artikel10 from "../assets/img/artikel/edukasi8(1).jpeg";
 
 const ArtikelPage = () => {
-  return ( 
-    <div className="artikel-page">  
+  return (
+    <div className="artikel-page">
       <div className="artikel min-vh-100">
         <Container>
           <Row>
             <Col>
-              <h1 className="fw-bold text-center animate__animated animate__fadeInUp">Semua Artikel</h1>
-              <p className="text-center animate__animated animate__fadeInUp">Kumpulan Artikel Lingkungan Yang Bisa Anda Baca</p>
+              <h1 className="fw-bold text-center animate__animated animate__fadeInUp">
+                Semua Artikel
+              </h1>
+              <p className="text-center animate__animated animate__fadeInUp">
+                Kumpulan Artikel Lingkungan Yang Bisa Anda Baca
+              </p>
             </Col>
           </Row>
           <Row>
-{/* MISAL MAU HAPUS KONTEN GANTI BARU AWALANE DARI SINI AJA */}
-          {semuaArtikel.map((artikel) => {
-            return (
-              <Col key={artikel.id} className="shadow rounded">
-                <img src={artikel.image} alt="" className="w-100 mb-3 rounded" />
-                <h5 className="mb-2 px-3">{artikel.title}</h5>
-                <p className="text-muted small mb-4 px-3">{artikel.date}</p>
-                <div className=" px-3 pb-3">
-                  <button className="btn rounded-10 me-2 mb-xs-0 mb-2"
-                  style={{ backgroundColor: '#0e516d', color: '#ffff' }}>{artikel.readmore}</button>
+            <div class="artikel" id="artikel">
+              <div class="container">
+                <div class="box-artikel">
+                  <div class="box">
+                    <img src={Artikel1} alt="" />
+                    <h3>Mengurangi Penggunaan Sampah Sehari-hari</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/mengurangi-sampah-sehari-hari.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel2} alt="" />
+                    <h3>Giat Melakukan Reboisasi Pada Lingkungan Sekitar</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/giat-melakukan-reboisasi.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel3} alt="" />
+                    <h3>Kreatif!! Olah Sampah Plastik Jadi Bata</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/olah-sampah-plastik-jadi-bata.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel4} alt="" />
+                    <h3>Ketahui Sampah Sesuai Jenis-Jenisnya</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/pilah-sampah-sebelum-dibuang.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel5} alt="" />
+                    <h3>Menyulap Limbah Plastik Menjadi BBM</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/sulap-limbah-plastik-jadi-bbm.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel6} alt="" />
+                    <h3>Pemanfaatan Limbah Plastik Menjadi Paving</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/limbah-plastik-menjadi-paving.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel7} alt="" />
+                    <h3>Pilah Sampah Organik dan Non Organik</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/pilah-sampah-organik-dan-non-organik.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel10} alt="" />
+                    <h3>Ayo Sulap Botol Kaca Menjadi Vas Bunga</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/sulap-botol-kaca-menjadi-vas-bunga.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
+                  <div class="box">
+                    <img src={Artikel9} alt="" />
+                    <h3>Sulap Stik Eskrim Menjadi Kerajinan Tangan</h3>
+                    <button>
+                      <a
+                        href="https://mengurangisampahseharihari.blogspot.com/2023/06/sulap-stik-eskrim-menjadi-kerajinan.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Detail
+                      </a>
+                    </button>
+                  </div>
                 </div>
-              </Col>
-              );
-            })}
-{/* AKHIR HAPUS KONTEN SAMPE SINI  */}
+              </div>
+            </div>
           </Row>
         </Container>
-
       </div>
     </div>
-  ); 
+  );
 };
 
 export default ArtikelPage;
